4ffde8ca89ccee6f133913a8d6edc83757a69b1e
Michał Wąsowski <mikaka27@hotmail.com>
2024-09-19 14:51:27 +0200

    Attempt 2

--- lib/mnesia/test/mnesia_external_backend_test.erl
+++ lib/mnesia/test/mnesia_external_backend_test.erl
@@ -33,11 +33,15 @@ suite() -> [{ct_hooks,[{ts_install_cth,[{nodenames,1}]}]}].
 
 backup_and_restore_fails_with_external_backend(Config) when is_list(Config) ->
     Node = node(),
-    Data = [
-        #some_rec{some_id = a, some_int = 1, some_string = "something" },
-        #some_rec{some_id = b, some_int = 2, some_string = "anything"  },
-        #some_rec{some_id = c, some_int = 3, some_string = "everything"},
-        #some_rec{some_id = d, some_int = 4, some_string = "nothing"   }
+    Data1 = [
+        #some_rec{some_id = a, some_int = 1, some_string = "1"},
+        #some_rec{some_id = b, some_int = 2, some_string = "2"},
+        #some_rec{some_id = c, some_int = 3, some_string = "3"}
+    ],
+    Data2 = [
+        #some_rec{some_id = d, some_int = 4, some_string = "4"},
+        #some_rec{some_id = e, some_int = 5, some_string = "5"},
+        #some_rec{some_id = f, some_int = 6, some_string = "6"}
     ],
 
     [Node] = ?acquire_nodes(1, Config),
@@ -47,12 +51,36 @@ backup_and_restore_fails_with_external_backend(Config) when is_list(Config) ->
         {attributes, record_info(fields, some_rec)},
         {ext_ets, [Node]}
     ]),
+    {atomic, ok} = mnesia:add_table_index(table, #some_rec.some_int),
+    ok = mnesia:backup("bup0.BUP"),
 
     ok = mnesia:activity(transaction, fun() ->
-        lists:foreach(fun(Elem) -> mnesia:write(table, Elem, write) end, Data)
+        lists:foreach(fun(Elem) -> mnesia:write(table, Elem, write) end, Data1)
     end),
+    ok = mnesia:backup("bup1.BUP"),
 
-    ok = mnesia:backup("bk.BUP"),
-    ok = mnesia:install_fallback("bk.BUP"),
+    ok = mnesia:activity(transaction, fun() ->
+        lists:foreach(fun(Elem) -> mnesia:write(table, Elem, write) end, Data2)
+    end),
+    ok = mnesia:backup("bup2.BUP"),
 
-    ok = mnesia:start().
+    ok = load_backup("bup0.BUP"),
+    [] = mnesia:dirty_match_object(table, #some_rec{_ = '_'}),
+    [] = mnesia:dirty_index_read(table, 2, #some_rec.some_int),
+
+    ok = load_backup("bup1.BUP"),
+    Data1 = mnesia:dirty_match_object(table, #some_rec{_ = '_'}),
+    [#some_rec{some_id = b, some_int = 2, some_string = "2"}] = mnesia:dirty_index_read(table, 2, #some_rec.some_int),
+
+    ok = load_backup("bup2.BUP"),
+    Data = lists:append(Data1, Data2),
+    Data = mnesia:dirty_match_object(table, #some_rec{_ = '_'}),
+    [#some_rec{some_id = b, some_int = 2, some_string = "2"}] = mnesia:dirty_index_read(table, 2, #some_rec.some_int),
+    [#some_rec{some_id = e, some_int = 5, some_string = "5"}] = mnesia:dirty_index_read(table, 5, #some_rec.some_int).
+
+load_backup(BUP) ->
+    ok = mnesia:install_fallback(BUP),
+    stopped = mnesia:stop(),
+    timer:sleep(3000),
+    ok = mnesia:start(),
+    ok = mnesia:wait_for_tables([table], 5000).
