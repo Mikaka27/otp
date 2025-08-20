9f018326a2f6789a269275d62dbb278fea4216f4
Michał Wąsowski <michal@erlang.org>
2025-08-20 09:43:10 +0200

    Reproduction work

--- mr.erl
+++ mr.erl
@@ -5,21 +5,22 @@
 
 -define(NUM_RECORDS, 20000).
 
-run() ->
-    run(100).
-run(Retries) ->
-    PeersAndNodes = start_peers(10),
+run(NumWorkers, NumTablesPerWorker, Retries) ->
+    process_flag(trap_exit, true),
+    PeersAndNodes = start_peers(NumWorkers),
     {Peers, Nodes} = lists:unzip(PeersAndNodes),
     ok = start_mnesia(Nodes),
+    io:fwrite("Starting peers...~n"),
     ok = mnesia:start([{extra_db_nodes, Nodes}]),
-    ok = create_tables(Nodes),
-    ok = loop(PeersAndNodes, Retries),
+    io:fwrite("Creating tables...~n"),
+    ok = create_tables(Nodes, NumTablesPerWorker),
+    ok = loop(PeersAndNodes, NumTablesPerWorker, Retries),
     ok = stop_mnesia(Nodes),
     stopped = mnesia:stop().
 
 start_peers(NumWorkers) ->
     lists:map(fun(N) ->
-        {ok, Controller, Node} = peer:start(#{
+        {ok, Controller, Node} = peer:start_link(#{
             name => "worker_" ++ integer_to_list(N)
         }),
         {Controller, Node}
@@ -44,43 +45,49 @@ get_node_number(Node) ->
     [_, NodeNumber] = string:split(NodeName, "_"),
     NodeNumber.
 
-get_table_name(Node) ->
+get_table_names(Node, NumTables) ->
     NodeNumber = get_node_number(Node),
-    list_to_atom("table_" ++ NodeNumber).
+    lists:map(fun(N) -> list_to_atom("table_" ++ NodeNumber ++ "_" ++ integer_to_list(N)) end,
+              lists:seq(1, NumTables)).
 
-create_tables(Nodes) ->
+create_tables(Nodes, NumTablesPerWorker) ->
     lists:foldl(fun(Node, Acc) ->
-        Table = get_table_name(Node),
-        {atomic, ok} = mnesia:create_table(Table, [
-            {type, set},
-            {record_name, some_rec},
-            {attributes, record_info(fields, some_rec)},
-            {ram_copies, Acc ++ [Node]}
-        ]),
-        ok = mnesia:sync_dirty(fun() ->
-            lists:foreach(fun(Id) ->
-                mnesia:write(Table, #some_rec{some_id = Id, some_int = Id}, write)
-            end, lists:seq(1, ?NUM_RECORDS))
-        end),
+        TableNames = get_table_names(Node, NumTablesPerWorker),
+        lists:foreach(fun(Table) ->
+            io:fwrite("Creating table ~p...~n", [Table]),
+            {atomic, ok} = mnesia:create_table(Table, [
+                {type, set},
+                {record_name, some_rec},
+                {attributes, record_info(fields, some_rec)},
+                {ram_copies, Acc ++ [Node]}
+            ]),
+            io:fwrite("Filling data in table ~p...~n", [Table]),
+            [mnesia:dirty_write(Table, #some_rec{some_id = Id, some_int = Id}) || Id <- lists:seq(1, ?NUM_RECORDS)],
+            io:fwrite("Table ~p created and filled with data.~n", [Table])
+        end, TableNames),
         Acc ++ [Node]
     end, [], Nodes),
     ok.
 
-verify_data_in_table(Node) ->
-    Table = get_table_name(Node),
-    Data = mnesia:dirty_match_object(Table, #some_rec{_ = '_'}),
-    ExpectedData = lists:map(fun(Id) -> #some_rec{some_id = Id, some_int = Id} end,
-                             lists:seq(1, ?NUM_RECORDS)),
-    case lists:sort(Data) of
-        ExpectedData ->
-            ok;
-        _ ->
-            {error, {unexpected_data, Data}}
-    end.
+verify_data_in_tables(Node, NumTablesPerWorker) ->
+    TableNames = get_table_names(Node, NumTablesPerWorker),
+    lists:foreach(fun(Table) ->
+        Data = mnesia:dirty_match_object(Table, #some_rec{_ = '_'}),
+        ExpectedData = lists:map(fun(Id) -> #some_rec{some_id = Id, some_int = Id} end,
+                                 lists:seq(1, ?NUM_RECORDS)),
+        case lists:sort(Data) of
+            ExpectedData ->
+                ok;
+            _ ->
+                {error, {unexpected_data, Data}}
+        end
+    end, TableNames).
 
-remove_and_readd_node(Node, PeersAndNodes) ->
+remove_and_readd_node(Node, NumTablesPerWorker, PeersAndNodes) ->
+    StartTime = erlang:monotonic_time(millisecond),
     Tables = mnesia:system_info(tables),
     ok = rpc:call(Node, mnesia, lkill, []),
+    io:fwrite("Removing node ~p...~n", [Node]),
     Functions = lists:foldl(fun(schema, Acc) ->
                         Acc;
                     (Table, Acc) ->
@@ -98,9 +105,10 @@ remove_and_readd_node(Node, PeersAndNodes) ->
     {Peer, Node} = lists:keyfind(Node, 2, PeersAndNodes),
     true = exit(Peer, kill),
     WorkerName = "worker_" ++ get_node_number(Node),
-    {ok, NewPeer, NewNode} = peer:start(#{
+    {ok, NewPeer, NewNode} = peer:start_link(#{
         name => WorkerName
     }),
+    io:fwrite("Restarting node ~p...~n", [NewNode]),
     {_, AllNodes} = lists:unzip(PeersAndNodes),
     OtherNodes = lists:delete(Node, AllNodes),
     ok = rpc:call(NewNode, mnesia, start, [[{extra_db_nodes, OtherNodes}]]),
@@ -108,8 +116,11 @@ remove_and_readd_node(Node, PeersAndNodes) ->
         Fun()
     end, Functions),
     try
-        ok = rpc:call(NewNode, mnesia, wait_for_tables, [[get_table_name(NewNode)], 120000]),
-        ok = rpc:call(NewNode, ?MODULE, verify_data_in_table, [NewNode])
+        io:fwrite("Waiting for tables to be ready on node ~p...~n", [NewNode]),
+        ok = rpc:call(NewNode, mnesia, wait_for_tables, [get_table_names(NewNode, NumTablesPerWorker), 120000]),
+        io:fwrite("Verifying data in tables on node ~p...~n", [NewNode]),
+        ok = rpc:call(NewNode, ?MODULE, verify_data_in_tables, [NewNode, NumTablesPerWorker]),
+        io:fwrite("Restart took: ~p~n", [erlang:monotonic_time(millisecond) - StartTime])
     catch
         C : R : ST ->
             io:fwrite("Error during re-adding node: ~p ~p:~p:~p~n", [Node, C, R, ST]),
@@ -118,13 +129,13 @@ remove_and_readd_node(Node, PeersAndNodes) ->
     end,
     lists:keyreplace(Peer, 1, PeersAndNodes, {NewPeer, NewNode}).
 
-loop(_, 0) ->
+loop(_, _, 0) ->
     ok;
-loop(PeersAndNodes, Retries) ->
+loop(PeersAndNodes, NumTablesPerWorker, Retries) ->
     {Peers, Nodes} = lists:unzip(PeersAndNodes),
-    Index = random:uniform(length(Nodes) - 1),
+    Index = rand:uniform(length(Nodes) - 1),
     %% Ensure we don't pick the first node, it's table has copy only on node 1
     Node = lists:nth(Index + 1, Nodes),
-    NewPeersAndNodes = remove_and_readd_node(Node, PeersAndNodes),
-    loop(NewPeersAndNodes, Retries - 1).
+    NewPeersAndNodes = remove_and_readd_node(Node, NumTablesPerWorker, PeersAndNodes),
+    loop(NewPeersAndNodes, NumTablesPerWorker, Retries - 1).
 
