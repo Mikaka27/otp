ca7ddcf29b8fa69314bd1e19ff2831f6a84159fd
Michał Wąsowski <michal@erlang.org>
2026-03-27 21:35:41 +0100

    Work

--- lib/common_test/test/ct_html_validation_SUITE.erl
+++ lib/common_test/test/ct_html_validation_SUITE.erl
@@ -34,6 +34,22 @@
 
 -include_lib("common_test/include/ct.hrl").
 
+-record(test, {
+    test_name :: string(),
+    suite_log_link :: string() | undefined,
+    label :: atom() | undefined,
+    start_date :: calendar:datetime(),
+    ok :: non_neg_integer(),
+    failed :: non_neg_integer(),
+    skipped :: non_neg_integer(),
+    user_skipped :: non_neg_integer(),
+    auto_skipped :: non_neg_integer(),
+    missing_suites :: non_neg_integer(),
+    node :: node(),
+    ct_log_link :: string(),
+    old_runs_link :: string() | link | undefined
+}).
+
 %%--------------------------------------------------------------------
 %% TEST SERVER CALLBACK FUNCTIONS
 %%--------------------------------------------------------------------
@@ -43,12 +59,11 @@
 %% instance, the tests need to be performed on a separate node (or
 %% there will be clashes with logging processes etc).
 %%--------------------------------------------------------------------
-init_per_suite(Config) ->
-    DataDir = ?config(data_dir, Config),
-    Config1 = ct_test_support:init_per_suite(Config),
-    SpecsDir1 = filename:join(DataDir, "specs1"),
-    SpecsDir2 = filename:join(DataDir, "specs2"),
-    [{specs_dir1,SpecsDir1},{specs_dir2,SpecsDir2} | Config1].
+init_per_suite(Config0) ->
+    DataDir = ?config(data_dir, Config0),
+    Config = ct_test_support:init_per_suite(Config0),
+    SpecsDir = filename:join(DataDir, "specs"),
+    [{specs_dir, SpecsDir} | Config].
 
 end_per_suite(Config) ->
     ct_test_support:end_per_suite(Config).
@@ -82,24 +97,30 @@ end_per_group(_GroupName, Config) ->
 %%%
 
 run_spec_twice(Config) ->
-    DataDir = ?config(data_dir, Config),
-    PrivDir = ?config(priv_dir, Config),
+    Specs = specs(?FUNCTION_NAME),
 
-    ct:pal("PrivDir: ~p~n", [PrivDir]),
+    setup_and_execute(?FUNCTION_NAME, specs(?FUNCTION_NAME), [], Config),
 
-    TCName = ?FUNCTION_NAME,
-    TestID = {userconfig,{?MODULE,atom_to_list(TCName)}},
-    % TestTerms = [TestID,{spec,Specs},{label,TCName}] ++ TestOpts,
+    % DataDir = ?config(data_dir, Config),
+    % PrivDir = ?config(priv_dir, Config),
 
-    Opts0 = ct_test_support:get_opts(Config),
-    FName = fname(specs_dir1, "simple_spec1", Config),
-    % Specs = [{spec, [FName]}, {spec, [FName]}],
-    Specs = [TestID,{spec, [FName]},{label,TCName}],
+    % ct:pal("PrivDir: ~p~n", [PrivDir]),
 
-    Opts = ct_test_support:get_overwritten_opts(Opts0 ++ Specs),
+    % TCName = ?FUNCTION_NAME,
+    % TestID = {userconfig, {?MODULE, atom_to_list(TCName)}},
+    % % TestTerms = [TestID,{spec,Specs},{label,TCName}] ++ TestOpts,
 
-    _ = ct_test_support:run_ct_run_test(Opts, Config),
-    validate_html_files(PrivDir).
+    % Opts0 = ct_test_support:get_opts(Config),
+    % FName = fname(specs_dir, "all_suites_spec", Config),
+    % % Specs = [{spec, [FName]}, {spec, [FName]}],
+    % Specs = [TestID, {spec, [FName]}, {label, TCName}],
+
+    % Opts = ct_test_support:get_overwritten_opts(Opts0 ++ Specs),
+
+    % _ = ct_test_support:run_ct_run_test(Opts, Config),
+    
+    % Expected = #test{test_name = }
+    % validate_html_files(PrivDir).
 
     %     ok ->
     %         ok;
@@ -130,17 +151,67 @@ run_spec_twice(Config) ->
 %%%-----------------------------------------------------------------
 
 check_parameter(TCID) ->
-    {ok,{config,TCID}}.
+    {ok, {config, TCID}}.
 
 read_config(TCID) ->
-    {ok,[{tcname,list_to_atom(TCID)}]}.
+    {ok, [{tcname, list_to_atom(TCID)}]}.
 
 fname(Tag, File, Config) ->
     filename:join(?config(Tag, Config), File).
 
+specs(run_spec_twice) ->
+    [tests1_spec].
+
+% -record(test, {
+%     test_name :: string(),
+%     suite_log_link :: string() | undefined,
+%     label :: atom() | undefined,
+%     start_date :: calendar:datetime(),
+%     ok :: non_neg_integer(),
+%     failed :: non_neg_integer(),
+%     skipped :: non_neg_integer(),
+%     user_skipped :: non_neg_integer(),
+%     auto_skipped :: non_neg_integer(),
+%     missing_suites :: non_neg_integer(),
+%     node :: node(),
+%     ct_log_link :: string(),
+%     old_runs_link :: string() | link | undefined
+% }).
+
+get_default_expected(Config) ->
+    #test{start_date = calendar:local_time(),
+          node = ?config(ct_node, Config)}.
+
+expected(tests1_spec, Label, Config) ->
+    Test = get_default_expected(Config),
+    [Test#test{test_name = ?config(data_dir, Config) ++ ".tests1",
+              label = Label,
+              ok = 32,
+              failed = 0,
+              skipped = 0,
+              user_skipped = 0,
+              auto_skipped = 0,
+              missing_suites = 0
+             }].
+
+modify_expected(Expected0, run_spec_twice) ->
+    WithLink = lists:map(fun(E) -> E#test{old_runs_link = link} end, Expected0),
+    Duplicated = lists:foldl(fun(E, Acc) -> [E, E | Acc] end, [], Expected0),
+    {WithLink, Duplicated}.
+
+setup_and_execute(TCName, Specs, TestOpts, Config) ->
+    TestID = {userconfig, {?MODULE, atom_to_list(TCName)}},
+    TestTerms = [TestID, {spec, Specs}, {label, TCName}] ++ TestOpts,
+
+    Opts0 = ct_test_support:get_opts(Config),
+    Opts = ct_test_support:get_overwritten_opts(Opts0 ++ TestTerms),
+
+    ct_test_support:run_ct_run_test(Opts, Config).
+
 validate_html_files(LogDir) ->
     MainIndex = filename:join(LogDir, "index.html"),
-    validate_index_html_file(MainIndex, "").
+    % Expected = #test{test_name = }
+    validate_index_html_file(MainIndex, []).
     % %% Find all HTML files in the log directory
     % HtmlFiles = filelib:wildcard(filename:join(LogDir, "**/*.html")),
     % ct:pal("Found ~p HTML files to validate", [length(HtmlFiles)]),
@@ -191,13 +262,18 @@ validate_html_files(LogDir) ->
 validate_index_html_file(Path, ExpectedTests) ->
     case file:open(Path, [read]) of
         {ok, Fd} ->
-            {ok, _Acc, _} = collect_until("<tbody>\n", file:read_line(Fd), Fd, []);
+            {ok, _} = collect_until("<tbody>\n", file:read_line(Fd), Fd, []),
+            {ok, Result} = collect_until("</tbody>\n", file:read_line(Fd), Fd, []),
+            ct:pal("Result: ~p~n", [Result]),
+            Tests = parse_tests(Result, []),
+            ct:pal("Tests: ~p~n", [Tests]),
+            Tests;
         Other ->
             Other
     end.
 
 collect_until(Expected, {ok, Expected}, _Fd, Acc) ->
-    {ok, Acc, Expected};
+    {ok, lists:reverse(Acc)};
 collect_until(Expected, {ok, Other}, Fd, Acc) ->
     collect_until(Expected, file:read_line(Fd), Fd, [Other | Acc]);
 collect_until(_Expected, eof, _Fd, _Acc) ->
@@ -205,4 +281,136 @@ collect_until(_Expected, eof, _Fd, _Acc) ->
 collect_until(_Expected, {error, Other}, _Fd, _Acc) ->
     {error, Other}.
 
+parse_tests([], Tests) ->
+    lists:reverse(Tests);
+parse_tests(["<tr class=" ++ _ | Rest], Tests) ->
+    parse_tests(Rest, [#test{} | Tests]);
+parse_tests(["<td><a href=\"" ++ Line0 | Rest], [Test | Tests]) ->
+    [Link, Line1] = string:split(Line0, "\">"),
+    [Name, _] = string:split(Line1, "<"),
+    case string:split(Line1, "<") of
+        ["CT Log", _] ->
+            parse_tests(Rest, [Test#test{ct_log_link = Link} | Tests]);
+        [Name, _] ->
+            parse_tests(Rest, [Test#test{test_name = Name, suite_log_link = Link} | Tests])
+    end;
+parse_tests(["<td align=center><b>" ++ Line | Rest], [Test | Tests]) ->
+    [Label, _] = string:split(Line, "<"),
+    parse_tests(Rest, [Test#test{label = list_to_atom(Label)} | Tests]);
+parse_tests(["<td>" ++ Line | Rest], [#test{start_date = undefined} = Test | Tests]) ->
+    [Date, _] = string:split(Line, "<"),
+    {ok, [_DayOfWeek, MonthName, Day, Year, Hour, Minute, Second], _} =
+        io_lib:fread("~s ~s ~d ~d ~d:~d:~d", Date),
+    Month = month_name_to_number(MonthName),
+    DateTime = {{Year, Month, Day}, {Hour, Minute, Second}},
+    parse_tests(Rest, [Test#test{start_date = DateTime} | Tests]);
+parse_tests(["<td align=right>" ++ Line | Rest], [#test{ok = undefined} = Test | Tests]) ->
+    [Ok, _] = string:split(Line, "<"),
+    parse_tests(Rest, [Test#test{ok = list_to_integer(Ok)} | Tests]);
+parse_tests(["<td align=right>" ++ Line | Rest], [#test{failed = undefined} = Test | Tests]) ->
+    [Failed, _] = string:split(Line, "<"),
+    parse_tests(Rest, [Test#test{failed = list_to_integer(Failed)} | Tests]);
+parse_tests(["<td align=right>" ++ Line | Rest], [#test{skipped = undefined} = Test0 | Tests]) ->
+    [All, _] = string:split(Line, "<"),
+    [Skipped, Other0] = string:split(All, "("),
+    [UserSkipped, Other1] = string:split(Other0, "/"),
+    [AutoSkipped, _] = string:split(Other1, ")"),
+    Test = Test0#test{skipped = list_to_integer(string:trim(Skipped)),
+                      user_skipped = list_to_integer(UserSkipped),
+                      auto_skipped = list_to_integer(AutoSkipped)},
+    parse_tests(Rest, [Test | Tests]);
+parse_tests(["<td align=right>" ++ Line | Rest], [#test{missing_suites = undefined} = Test | Tests]) ->
+    [MissingSuites, _] = string:split(Line, "<"),
+    parse_tests(Rest, [Test#test{missing_suites = list_to_integer(MissingSuites)} | Tests]);
+parse_tests(["<td align=right>" ++ Line | Rest], [#test{node = undefined} = Test | Tests]) ->
+    [Node, _] = string:split(Line, "<"),
+    parse_tests(Rest, [Test#test{node = list_to_atom(Node)} | Tests]);
+parse_tests(["<td>none</td>\n" | Rest], [#test{old_runs_link = undefined} = Test | Tests]) ->
+    parse_tests(Rest, [Test | Tests]);
+parse_tests(["</tr>\n" | Rest], Tests) ->
+    parse_tests(Rest, Tests).
 
+month_name_to_number("Jan") -> 1;
+month_name_to_number("Feb") -> 2;
+month_name_to_number("Mar") -> 3;
+month_name_to_number("Apr") -> 4;
+month_name_to_number("May") -> 5;
+month_name_to_number("Jun") -> 6;
+month_name_to_number("Jul") -> 7;
+month_name_to_number("Aug") -> 8;
+month_name_to_number("Sep") -> 9;
+month_name_to_number("Oct") -> 10;
+month_name_to_number("Nov") -> 11;
+month_name_to_number("Dec") -> 12.
+
+compare_tests([#test{test_name = TestName,
+                     label = Label,
+                     start_date = Date1,
+                     ok = Ok,
+                     failed = Failed,
+                     skipped = Skipped,
+                     user_skipped = UserSkipped,
+                     auto_skipped = AutoSkipped,
+                     missing_suites = MissingSuites,
+                     node = Node
+                    } = Expected | Tests1],
+              [#test{test_name = TestName,
+                     suite_log_link = SuiteLink,
+                     label = Label,
+                     start_date = Date2,
+                     ok = Ok,
+                     failed = Failed,
+                     skipped = Skipped,
+                     user_skipped = UserSkipped,
+                     auto_skipped = AutoSkipped,
+                     missing_suites = MissingSuites,
+                     node = Node,
+                     ct_log_link = CtLogLink,
+                     old_runs_link = OldRunsLink
+                    } | Tests2]) ->
+    ok = compare_date(Date1, Date2),
+    ok = validate_links(Expected, SuiteLink, CtLogLink, OldRunsLink),
+    compare_tests(Tests1, Tests2).
+
+compare_date(Date, Date) ->
+    ok;
+compare_date(Date1, Date2) ->
+    S1 = calendar:datetime_to_gregorian_seconds(Date1),
+    S2 = calendar:datetime_to_gregorian_seconds(Date2),
+    case abs(S2 - S1) of
+        Diff when Diff =< 30 -> ok
+    end.
+
+validate_links(Expected, SuiteLink, CtLogLink, OldRunsLink) ->
+    SuiteLinkParts = string:split(SuiteLink, "/", all),
+    4 = length(SuiteLinkParts),
+    ok = validate_link_parts(Expected, SuiteLinkParts),
+
+    CtLogLinkParts = string:split(CtLogLink, "/", all),
+    2 = length(CtLogLinkParts),
+    ok = validate_link_parts(Expected, CtLogLinkParts).
+
+validate_link_parts(_Expected, []) ->
+    ok;
+validate_link_parts(#test{start_date = ExpectedDate, node = ExpectedNode} = Expected,
+                    ["ct_run." ++ Part | Rest]) ->
+    [Node, Date] = string:split(Part, ".", all),
+    ExpectedNode = list_to_atom(Node),
+    ParsedDate = parse_link_date(Date),
+    compare_date(ExpectedDate, ParsedDate),
+    validate_link_parts(Expected, Rest);
+validate_link_parts(#test{start_date = ExpectedDate} = Expected, ["run." ++ Date | Rest]) ->
+    ParsedDate = parse_link_date(Date),
+    compare_date(ExpectedDate, ParsedDate),
+    validate_link_parts(Expected, Rest);
+validate_link_parts(#test{test_name = TestName} = Expected, [Part | Rest]) ->
+    Part = TestName ++ ".logs",
+    validate_link_parts(Expected, Rest);
+validate_link_parts(_Expected, ["suite.log.html" | []]) ->
+    ok;
+validate_link_parts(_Expected, ["ctlog.html" | []]) ->
+    ok.
+
+parse_link_date(Date) ->
+    {ok, [Year, Month, Day, Hour, Minute, Second], _} = io_lib:fread("~d-~d-~d_~d.~d.~d", Date),
+    {{Year, Month, Day}, {Hour, Minute, Second}}.
